03eda7a6f7209cff988eea1fe9487c8427519f1f
JoaoVict0rVS <joao.victor.job1@gmail.com>
2025-04-06 02:51:10 +0000

    DESAFIO BATALHA NAVAL MESTRE - CONCLUIDO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -43,28 +43,106 @@ int linhas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; //Vetor par
                printf("%d ", matriz[x][y]);
             }
     }
-    return 0;
-}
 
+    int Hab;
+    int l1, l2;
+    int c1, c2;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int Cone[3][5] = {      // Matriz do cone
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1}
+        };
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    int Octaedro[3][5] = {     // Matriz do octaedro
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+        };
+
+    int Cruz[3][5] = {        // Matriz da cruz
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0}
+        };
+
+    // Escolha da habilidade e endereço
+    printf("\n\n"); 
+    printf("Escolha qual habilidade usar:\n1. Cone\n2. Octaedro\n3. Cruz\n");
+    scanf("%d", &Hab);
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("\n");
+    printf("Escolha em qual linha aplicar: ");
+    scanf("%d", &l1);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("\n");
+    printf("Escolha qual coluna usar: ");
+    scanf("%d", &c1);
+
+    l2 = l1 - 2;
+    c2 = c1 - 3;
+
+
+    if (Hab == 1){   // Chamada do cone
+        for (int cx = 0; cx < 3; cx++){
+                for (int cy = 0; cy < 5; cy++){
+                if (matriz[cx + l2][cy + c2] + Cone[cx][cy] == 3)
+            {
+                Cone[cx][cy] = matriz[cx + l2][cy + c2];
+            } else (
+                matriz[cx + l2][cy + c2] = Cone[cx][cy]);
+                }
+            }
+
+            } else if (Hab == 2){ // Chamada do octaedro
+                    for (int ox = 0; ox < 3; ox++) {
+                        for (int oy = 0; oy < 5; oy++) {
+                        if (matriz[ox + l2][oy + c2] + Octaedro[ox][oy] == 3)
+                    {
+                        Octaedro[ox][oy] = matriz[ox + l2][oy + c2];
+                    } else (
+                        matriz[ox + l2][oy + c2] = Octaedro[ox][oy]);
+                    }
+                }
+
+                } else if (Hab == 3){ // Chamada da cruz
+                                for (int crx = 0; crx < 3; crx++) {
+                                    for (int cry = 0; cry < 5; cry++) {
+                                    if (matriz[crx + l2][cry + c2] + Cruz[crx][cry] == 3)
+                                {
+                                    Cruz[crx][cry] = matriz[crx + l2][cry + c2];
+                                } else (
+                                    matriz[crx + l2][cry + c2] = Cruz[crx][cry]);
+                                    }
+                                }
+                    }
+        
+        
+          else {
+            printf("Opção inválida! Nenhuma habilidade aplicada.\n\n");
+          }
+
+// Chamada da matriz principal após habilidade
+
+          printf("  "); //Espaço para identação ficar correta no terminal
+          for (int i = 0; i < 10; i++) {   //Declaração das colunas
+            printf(" %c", linhas[i]);
+        }
+    
+        for(int j = 1, x = 0; j <= 10 && x < 10; j++, x++){ //Declaração linhas
+            
+            if (j < 10){
+            printf("\n %d ", j);
+            } else{
+            printf("\n%d ", j);
+            }
+                
+                for (int y = 0; y < 10; y++) {        
+                   printf("%d ", matriz[x][y]);
+                }
+        }
+
+
+            return 0;
+}
 
